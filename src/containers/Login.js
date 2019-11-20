250d8c39084343365acdf4c9afa9059273b79425
ianhunterharold <ianhunterharold@gmail.com>
2019-11-20 13:16:38 -0800

    styled login page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 
 class Login extends Component {
   state ={
@@ -9,6 +10,10 @@ class Login extends Component {
 
   handleInput = (e) => {
     this.setState({ [e.target.id]: e.target.value })
+  }
+
+  loggedIn = () => {
+    return !!localStorage.getItem('token')
   }
 
   userLogin = (e) => {
@@ -28,35 +33,50 @@ class Login extends Component {
         password: this.state.password.toLowerCase()
       })
     }).then(res => res.json())
-      .then (console.log)
+      .then (token => localStorage.setItem('token', token.jwt))
+      .then(console.log(localStorage))
   }
 
 
   render() {
     return (
       <div>
-        <form onSubmit={this.userLogin}>
-          <label>username</label>
-          <input 
-            type='text'
+        <Grid textAlign='center' style={{ height: '100vh' }}  veriticalalign='midle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2'color='blue' textAlign='center'>log-in to your account</Header>
+        <form size='large' onSubmit={this.userLogin}>  
+          <Segment stacked>
+          <Form.Input 
+            fluid
+            icon='user'
+            iconPosition='left'
+            type='username'
             id='username'
-            placeholder= 'username'
+            placeholder= 'Username'
             value={this.state.username}
             onChange={this.handleInput}
-            required
           />
-          <label>password</label>
-          <input 
-            type='text'
+          <Form.Input 
+            fluid
+            icon='lock'
+            iconPosition='left'
+            type='password'
             id='password'
-            placeholder= 'password'
+            placeholder= 'Password'
             value={this.state.password}
             onChange={this.handleInput}
-            required
           />
-          <button type='submit'>login</button>
+          <Button color='blue' fluid size='large' type='submit'>Login</Button>
+          </Segment>
         </form>
+        <Message>
+          New? <a>Sign Up</a>   
+          {/* will need pathing to signup page */}
+        </Message>
+        </Grid.Column>
+        </Grid>
       </div>
+
     )
   }
 
